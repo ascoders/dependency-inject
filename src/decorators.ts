e7bae4c1bb49987f4db19c19fe6e6d6d645a145e
紫益 <ziyi.hzy@alibaba-inc.com>
2017-06-17 19:09:58 +0800

    update

--- src/decorators.ts
+++ src/decorators.ts
@@ -3,7 +3,11 @@ import { injectSymbol } from './handlers'
 export default <T>(injectClass: T) => (target: any, propertyKey: string | symbol) => {
     // 这个字段用来存储所有可能从注入中获取的数据
     if (!target[injectSymbol]) {
-        target[injectSymbol] = new Map()
+        Object.defineProperty(target, injectSymbol, {
+            enumerable: true,
+            configurable: true,
+            value: new Map()
+        })
     }
 
     target[injectSymbol].set(propertyKey, injectClass)
