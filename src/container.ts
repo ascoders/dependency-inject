0ac7bdce8888e3cfcb5b75332df9d891f566248f
紫益 <ziyi.hzy@alibaba-inc.com>
2017-06-17 22:50:56 +0800

    多次加载数据可以互通

--- src/container.ts
+++ src/container.ts
@@ -1,4 +1,5 @@
 import { injectSymbol } from './handlers'
+import { globalState } from "./utils"
 
 interface normalObject {
     [x: string]: any
@@ -9,14 +10,12 @@ export declare type ObjectType<T> = {
 }
 
 export default class Container {
-    private instances = new WeakMap()
-
     /**
      * 为类设置对应的实例，依赖注入时，将会从 set 时赋值的实例中寻找
      */
     public set<T extends normalObject>(setClass: ObjectType<T>, instance: T) {
-        if (!this.instances.has(setClass)) {
-            this.instances.set(setClass, instance)
+        if (!globalState.instances.has(setClass)) {
+            globalState.instances.set(setClass, instance)
         }
     }
 
@@ -24,11 +23,11 @@ export default class Container {
      * 拿到类的实例，如果提前 set 实例，优先获取此实例
      */
     public get<T>(getClass: ObjectType<T>): T {
-        if (!this.instances.has(getClass)) {
+        if (!globalState.instances.has(getClass)) {
             throw new Error(`${getClass.name} 未注册。先使用 set 方法注册，再使用 get 获取`)
         }
 
-        const instance = this.instances.get(getClass)
+        const instance = globalState.instances.get(getClass)
 
         // 如果这个类没有 inject 过，就不会存在这个 symbol，直接返回实例
         if (!instance[injectSymbol]) {
@@ -41,7 +40,7 @@ export default class Container {
                 enumerable: true,
                 configurable: true,
                 get: () => {
-                    return this.instances.get(injectClass)
+                    return globalState.instances.get(injectClass)
                 },
                 set: (newValue: any) => {
                     instance.propertyKey = newValue
---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+const tag = "ascoders-dependency-inject"
+
+const globalOrWindow = (typeof self === 'object' && self.self === self && self) ||
+  (typeof global === 'object' && global.global === global && global) ||
+  this
+
+class GlobalState {
+  /**
+   * 所有通过 get 访问过的 class 实例
+   */
+  instances = new WeakMap()
+}
+
+let globalState = new GlobalState()
+
+if (globalOrWindow[tag]) {
+  globalState = globalOrWindow[tag]
+} else {
+  globalOrWindow[tag] = globalState
+}
+
+export { globalState }
