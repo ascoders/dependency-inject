a84a784e1502b4da45edb6437d59b6f57f26b83d
紫益 <ziyi.hzy@alibaba-inc.com>
2017-07-10 22:08:35 +0800

    deploy 1.0.18

--- src/helper.ts
+++ src/helper.ts
@@ -1,16 +1,22 @@
 import Container from "./container"
 
+export declare interface IObjectType<T> {
+  new (): T
+}
+
+export declare type ICombineActions<T> = {
+  [P in keyof T]?: IObjectType<T[P]>
+}
+
 /**
  * 快速实例化，key: string value: Class
  * 可以将内部 inject 正确注入
  */
-export function injectFactory(obj: {
-  [name: string]: any
-}) {
+export function injectFactory<T>(obj: ICombineActions<T>): T {
   const container = new Container()
   Object.keys(obj).forEach(key => {
-    let instance = new obj[key]()
-    container.set(obj[key], instance)
+    let instance: any = new obj[key]()
+    container.set((obj as any)[key], instance)
   })
 
   const injectObj = Object.keys(obj).reduce((result, key) => {
